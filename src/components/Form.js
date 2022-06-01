62aae12ed2bc3054c9d68a0b2badf442ac9436c6
ReitoSerizawa <s.reitiger@gmail.com>
2022-06-01 11:44:39 -0400

    form ver1

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,130 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Form () {
+
+    // const [formData, setFormData] = useState({yoga_type:  "", intensity: "", schedule_date: "", schedule_time: ""})
+    const [yogaType, setYogaType] = useState("")
+    const [intensity, setIntensity] = useState("")
+    const [scheduleDate, setScheduleDate] = useState ("")
+    const [timeStart, setTimeStart] = useState("")
+    const [timeEnd, setTimeEnd] = useState("")
+
+    // function handleChange (e) {
+    //     let name = e.target.name
+    //     let value = e.target.value
+
+    //     setFormData({
+    //         ...formData,
+    //         [name]: value,
+    //         schedule_time: timeStart + "-" + timeEnd
+    //     })
+
+    // }
+
+    function handleChangeType (e) {
+        setYogaType(e.target.value)
+    }
+
+    function handleChangeIntensity (e) {
+        setIntensity(e.target.value)
+    }
+
+    function handleChangeDate (e) {
+        setScheduleDate(e.target.value)
+    }
+
+    function handleChaneOne (e) {
+        setTimeStart(e.target.value)
+    }
+
+    function handleChaneTwo (e) {
+        setTimeEnd(e.target.value)
+    }
+
+    function handleSubmit (e) {
+        e.preventDefault()
+        console.log({
+            yoga_type: yogaType,
+            intensity: intensity,
+            schedule_date: scheduleDate,
+            schedule_time: timeStart + "-" + timeEnd
+        })
+    }
+
     return (
-    <h1>Form</h1>
+        <div className="main">
+
+            <h1 style={{padding:"15px"}}>New Class</h1>
+            <hr />
+
+            <form className="form" id='form' onSubmit={handleSubmit}>
+
+                
+                <div className="row justify-content-center" style={{padding:"15px"}}>
+
+                    <label className="col-sm-1 col-form-label" for="name">Type</label>
+
+                    <div className="col-sm-5">
+                        <input value={yogaType} onChange={handleChangeType} className="form-control" type="text" name="yoga_type" placeholder="ex. Vinyasa Yoga"/>
+                    </div>
+
+                </div>
+
+                <hr />
+
+                <div className="row justify-content-center" style={{padding:"15px"}}>
+
+                    <label className="col-sm-1 col-form-label" for="name">Intensity</label>
+
+                    <div className="col-sm-5">
+                        <select value={intensity} onChange={handleChangeIntensity} className="form-control" name="intensity">
+                            <option value="C2">C2</option>
+                            <option value="C1">C1</option>
+                            <option value="B2">B2</option>
+                            <option value="B1">B1</option>
+                            <option value="A2">A2</option>
+                            <option value="A1">A1</option>
+                        </select>
+                    </div>
+
+                </div>
+
+                <hr />
+
+                <div className="row justify-content-center" style={{padding:"15px"}}>
+                    
+                    <label className="col-sm-1 col-form-label" for="name">Date</label>
+                    
+                    <div className="col-sm-5">
+                        <input value={scheduleDate} onChange={handleChangeDate} placeholder="Select date" name="schedule_date" type="date" className="form-control"/>
+                    </div>
+
+                </div>
+
+                <hr />
+
+                <div className="row justify-content-center" style={{padding:"15px"}}>
+
+                    <label className="col-sm-1 col-form-label" for="name">Start</label>
+
+                    <div className="col-sm-2">
+                        <input value={timeStart} onChange={handleChaneOne} type="time" id="example" className="form-control" min="7:00" max="21:00" step="60"/>
+                    </div>
+
+                    <label className="col-sm-1 col-form-label" for="name">End</label>
+
+                    <div className="col-sm-2">
+                        <input value={timeEnd} onChange={handleChaneTwo} type="time" id="example" className="form-control" min="7:00" max="21:00" step="60"/>
+                    </div>
+
+                </div>
+
+                <hr />
+
+                <button type="submit" class="btn btn-outline-primary">Submit</button>
+
+            </form>
+        </div>
     )
 }
 

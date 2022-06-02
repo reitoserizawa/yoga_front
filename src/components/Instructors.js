1df08805d56c719b02cab6c0d1ec02acbd62077d
ReitoSerizawa <s.reitiger@gmail.com>
2022-06-02 09:50:23 -0400

    modal, instructors added

--- src/components/Instructors.js
+++ src/components/Instructors.js
@@ -1,39 +1,43 @@
 import React from 'react';
+import InstructorsItem from './InstructorsItem'
 
 function Instructors ({instructors}) {
 
-    var output = "<div class='row'>";
+    // let output = "<div class='row'>";
 
-    for(var i=0;i<instructors.length;i++) {
-       if((i%3)==0)
-       {
-          output += "</div><div class='row'>" + "<div class='col-md-4'><!-- content --></div>";
-       }
-       else
-       {
-          output += "<div class='col-md-4'><!-- content --></div>";
-       }
-    }
+    // instructors.map(instructor => {
 
-    if((i%3)!=0) {
-       output += "</div><div class='row'>";
-    }
+    // for(var i=0;i<instructors.length;i++) {
+    //    if((i%3)==0)
+    //    {
+    //       output += "</div><div class='row'>" + "<div class='col-md-4'>{divs}</div>";
+    //    }
+    //    else
+    //    {
+    //       output += "<div class='col-md-4'>{divs}</div>";
+    //    }
+    // }
 
-    console.log(instructors.length)
-
-    
-
+    // if((i%3)!=0) {
+    //    output += "</div><div class='row'>";
+    // }
+    // })
 
     return (
     <div className="main">
 
         <h1 style={{padding:"15px"}}>Instructors</h1>
         <hr />
-        <div class="row">
-  <div class="col-md-4">!-- content --</div>
-  <div class="col-md-4">!-- content --</div>
-  <div class="col-md-4">!-- content --</div>
-</div>
+
+        <div class="container">
+            <div class="row">
+
+            {instructors.map(instructor => {
+            return <InstructorsItem key={instructor.id} name={instructor.first_name + " " + instructor.last_name} experience={instructor.years_of_experience} location={instructor.studio_location} image={instructor.avatar} bio={instructor.bio} />
+            })}
+
+            </div>
+        </div>
 
     
 
